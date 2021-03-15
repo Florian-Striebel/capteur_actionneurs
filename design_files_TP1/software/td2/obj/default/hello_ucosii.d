48eae09804e662db157c9e1f27cda12f24275b3c
Striebel <florian.striebel@live.fr>
2021-03-15 18:12:50 +0100

    td02

---
+++ design_files_TP1/software/td2/obj/default/hello_ucosii.d
@@ -0,0 +1,41 @@
+obj/default/hello_ucosii.o: hello_ucosii.c ../td2_bsp//HAL/inc/includes.h \
+ ../td2_bsp//HAL/inc/os_cpu.h ../td2_bsp//HAL/inc/sys/alt_irq.h \
+ ../td2_bsp//HAL/inc/nios2.h ../td2_bsp//HAL/inc/alt_types.h \
+ ../td2_bsp/system.h ../td2_bsp/linker.h ../td2_bsp//UCOSII/inc/os_cfg.h \
+ ../td2_bsp//HAL/inc/sys/alt_alarm.h ../td2_bsp//HAL/inc/sys/alt_llist.h \
+ ../td2_bsp//HAL/inc/priv/alt_alarm.h ../td2_bsp//UCOSII/inc/ucos_ii.h \
+ ../td2_bsp//HAL/inc/os_cpu.h \
+ ../td2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../td2_bsp//HAL/inc/io.h ../td2_bsp//HAL/inc/alt_types.h
+
+../td2_bsp//HAL/inc/includes.h:
+
+../td2_bsp//HAL/inc/os_cpu.h:
+
+../td2_bsp//HAL/inc/sys/alt_irq.h:
+
+../td2_bsp//HAL/inc/nios2.h:
+
+../td2_bsp//HAL/inc/alt_types.h:
+
+../td2_bsp/system.h:
+
+../td2_bsp/linker.h:
+
+../td2_bsp//UCOSII/inc/os_cfg.h:
+
+../td2_bsp//HAL/inc/sys/alt_alarm.h:
+
+../td2_bsp//HAL/inc/sys/alt_llist.h:
+
+../td2_bsp//HAL/inc/priv/alt_alarm.h:
+
+../td2_bsp//UCOSII/inc/ucos_ii.h:
+
+../td2_bsp//HAL/inc/os_cpu.h:
+
+../td2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../td2_bsp//HAL/inc/io.h:
+
+../td2_bsp//HAL/inc/alt_types.h:
---
+++ design_files_TP1/software/td2/reflex_game_ucosii.c
@@ -0,0 +1,142 @@
+/*************************************************************************
+* Copyright (c) 2004 Altera Corporation, San Jose, California, USA.      *
+* All rights reserved. All use of this software and documentation is     *
+* subject to the License Agreement located at the end of this file below.*
+**************************************************************************
+* Description:                                                           *
+* The following is a simple hello world program running MicroC/OS-II.The *
+* purpose of the design is to be a very simple application that just     *
+* demonstrates MicroC/OS-II running on NIOS II.The design doesn't account*
+* for issues such as checking system call return codes. etc.             *
+*                                                                        *
+* Requirements:                                                          *
+*   -Supported Example Hardware Platforms                                *
+*     Standard                                                           *
+*     Full Featured                                                      *
+*     Low Cost                                                           *
+*   -Supported Development Boards                                        *
+*     Nios II Development Board, Stratix II Edition                      *
+*     Nios Development Board, Stratix Professional Edition               *
+*     Nios Development Board, Stratix Edition                            *
+*     Nios Development Board, Cyclone Edition                            *
+*   -System Library Settings                                             *
+*     RTOS Type - MicroC/OS-II                                           *
+*     Periodic System Timer                                              *
+*   -Know Issues                                                         *
+*     If this design is run on the ISS, terminal output will take several*
+*     minutes per iteration.                                             *
+**************************************************************************/
+
+
+#include <stdio.h>
+#include "includes.h"
+#include "altera_avalon_pio_regs.h"
+#include "system.h"
+
+/* Definition of Task Stacks */
+#define   TASK_STACKSIZE       2048
+OS_STK    task1_stk[TASK_STACKSIZE];
+OS_STK    task2_stk[TASK_STACKSIZE];
+/* Definition of Task Priorities */
+
+#define TASK1_PRIORITY      1
+#define TASK2_PRIORITY      2
+
+int lut[10] = {0x40,0x79,0x24,0x30,0x19,0x12,0x2,0x78,0x0,0x10};
+void* msgMail;
+OS_EVENT* event_queue=NULL;
+
+void display_hex(int i){
+	IOWR_ALTERA_AVALON_PIO_DATA (HEX0_BASE, lut[i%10]);
+	IOWR_ALTERA_AVALON_PIO_DATA (HEX1_BASE, lut[(i/10)%10]);
+	IOWR_ALTERA_AVALON_PIO_DATA (HEX2_BASE, lut[(i/100)%10]);
+	IOWR_ALTERA_AVALON_PIO_DATA (HEX3_BASE, lut[(i/1000)%10]);
+	IOWR_ALTERA_AVALON_PIO_DATA (HEX4_BASE, lut[(i/10000)%10]);
+	IOWR_ALTERA_AVALON_PIO_DATA (HEX5_BASE, lut[(i/100000)%10]);
+}
+/* Prints "Hello World" and sleeps for three seconds */
+
+void task1_mail(void* pdata)
+{
+  int compteur= 0;
+  msgMail=&compteur;
+  mail_box=OSMboxCreate(msgMail);
+  while (1)
+  {
+    printf("Hello from task1\n");
+    compteur+=1;
+    OSMboxPost(mail_box,msgMail);
+    OSTimeDlyHMSM(0, 0, 4, 0);
+  }
+}
+/* Prints "Hello World" and sleeps for three seconds */
+void task2_mail(void* pdata)
+{
+
+  while (1)
+  {
+    printf("Hello from task2\n");
+    int compteur= OSMboxAccept(mail_box);
+    display_hex(compteur);
+    OSTimeDlyHMSM(0, 0, 2, 0);
+  }
+}
+/* The main function creates two task and starts multi-tasking */
+int main(void)
+{
+
+  OSTaskCreateExt(task_button_push,
+                   NULL,
+                   (void *)&task1_mail_stk[TASK_STACKSIZE-1],
+                   TASK1_PRIORITY,
+                   TASK1_PRIORITY,
+                   task1_stk,
+                   TASK_STACKSIZE,
+                   NULL,
+                   0);
+
+
+   OSTaskCreateExt(task_,
+                   NULL,
+                   (void *)&task2_stk[TASK_STACKSIZE-1],
+                   TASK2_PRIORITY,
+                   TASK2_PRIORITY,
+                   task2_stk,
+                   TASK_STACKSIZE,
+                   NULL,
+                   0);
+
+  OSStart();
+  return 0;
+}
+
+/******************************************************************************
+*                                                                             *
+* License Agreement                                                           *
+*                                                                             *
+* Copyright (c) 2004 Altera Corporation, San Jose, California, USA.           *
+* All rights reserved.                                                        *
+*                                                                             *
+* Permission is hereby granted, free of charge, to any person obtaining a     *
+* copy of this software and associated documentation files (the "Software"),  *
+* to deal in the Software without restriction, including without limitation   *
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,    *
+* and/or sell copies of the Software, and to permit persons to whom the       *
+* Software is furnished to do so, subject to the following conditions:        *
+*                                                                             *
+* The above copyright notice and this permission notice shall be included in  *
+* all copies or substantial portions of the Software.                         *
+*                                                                             *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  *
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    *
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE *
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      *
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     *
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         *
+* DEALINGS IN THE SOFTWARE.                                                   *
+*                                                                             *
+* This agreement shall be governed in all respects by the laws of the State   *
+* of California and by the laws of the United States of America.              *
+* Altera does not recommend, suggest or require that this reference design    *
+* file be used in conjunction or combination with any other product.          *
+******************************************************************************/
